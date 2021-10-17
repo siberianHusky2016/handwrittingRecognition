e6a8d8cc9868fdd1dcc2cce97eb6620aca8fb7f0
Kevin Zhou <60158870+siberianHusky2016@users.noreply.github.com>
2021-10-16 23:42:32 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+loss, accuracy = model.evaluate(x_test, y_test)
+print("accuracy = ", accuracy)
+print("loss = ", loss)
+
+model.save('digits.model')
+
+
+mapNumToName = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine"
+}
+
+
+for i in range(0, 10):
+    image = cv.imread(f'./data/{mapNumToName[i]}.png')[:, :, 0]
+    image = np.invert(np.array([image]))
+
+    prediction = model.predict(image)
+    print("Guess: ", np.argmax(prediction))
+
+    plt.imshow(image[0], cmap=plt.cm.binary)
+    plt.show()
